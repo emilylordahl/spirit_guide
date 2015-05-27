9e3abaf2e53dd32799364595570447860e527bd5
Emily Lordahl <contact@emilylordahl.com>
2015-05-26 21:08:23 -0400

    custom map styles

--- app/assets/javascripts/backbone/views/map.js
+++ app/assets/javascripts/backbone/views/map.js
@@ -5,13 +5,48 @@ App.Views.Map = Backbone.View.extend({
   initialize: function() {
     this.$el.empty();
 
+    var MY_MAPTYPE_ID = 'custom_style';
+
+    var featureOpts = [
+      {
+      stylers: [
+        { hue: '#A0D9C4' },
+        { saturation: -20 }
+      ]
+    },{
+      featureType: 'road',
+      elementType: 'geometry',
+      stylers: [
+        { lightness: 100 },
+        { visibility: 'simplified' }
+      ]
+    },{
+      featureType: 'road',
+      elementType: 'labels',
+      stylers: [
+        { visibility: 'on' }
+      ]
+    }
+  ];
+
     var mapOptions = {
       zoom: 15, 
       center: {lat: parseFloat(this.model.latitude), lng: parseFloat(this.model.longitude)},
-      mapTypeId: google.maps.MapTypeId.ROADMAP
+      scrollwheel: false,
+      mapTypeId: MY_MAPTYPE_ID,
+      mapTypeControlOptions: {
+        mapTypeIds: [google.maps.MapTypeId.ROADMAP, MY_MAPTYPE_ID]
+      }
   	};
 
+    var styledMapOptions = {
+      name: 'Custom Style'
+    };
+
+    var customMapType = new google.maps.StyledMapType(featureOpts, styledMapOptions);
+
     this.map = new google.maps.Map(this.el, mapOptions);
+    this.map.mapTypes.set(MY_MAPTYPE_ID, customMapType);
     this.render();
 
   },
@@ -19,15 +54,15 @@ App.Views.Map = Backbone.View.extend({
   render: function() {
     var marker = new google.maps.Marker({
       map: this.map,
-      position: {lat: parseFloat(this.model.latitude), lng: parseFloat(this.model.longitude)},
+      position: { lat: parseFloat(this.model.latitude), lng: parseFloat(this.model.longitude) },
       animation: google.maps.Animation.DROP
     });
 
     var infoWindow = new google.maps.InfoWindow({
-      content: this.model.name
+      content: '<h2>' + this.model.name + '</h2>' + '<p>' + this.model.address + '</p>'
     });
 
-    google.maps.event.addListener(marker, "click", function(){
+    google.maps.event.addListener(marker, 'click', function(){
       infoWindow.open(this.map, marker)
     });
 
