ecb55035cb7f42630438c711cc6f09fa14fbe183
Emily Lordahl <contact@emilylordahl.com>
2015-06-04 22:31:24 -0400

    env variables

--- config/initializers/yelp.rb
+++ config/initializers/yelp.rb
@@ -2,8 +2,8 @@ require 'yelp'
 
 YOUR_CONSUMER_KEY = '2FtU74xFLOX1XSxj-WoTfg'
 YOUR_CONSUMER_SECRET = 'tFRTS-FfKvqmrAF5FoiH3L51fII'
-YOUR_TOKEN = 'GWM5y9eRNAyRnIJqUD9_1N9gKRFNbgIg'
-YOUR_TOKEN_SECRET = 'wG36Tb77XaZB5nK5-GoKTH5DaQs'
+YOUR_TOKEN = 'eB0LCgYRp4bHHslyZpCUaCMV2g4cjRgk'
+YOUR_TOKEN_SECRET = 'P_wY_Za4KVcMEECBioUrOAssh4g'
 
 Yelp.client.configure do |config|
   config.consumer_key = YOUR_CONSUMER_KEY
